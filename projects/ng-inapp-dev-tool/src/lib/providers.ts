ee084540329fc0e63b12b7d747623f1701c75db5
abdhulla-k <abdhullak149@gmail.com>
2026-05-02 22:59:11 +0530

    feat: implement plugin architecture and overview plugin landing page

--- projects/ng-inapp-dev-tool/src/lib/providers.ts
+++ projects/ng-inapp-dev-tool/src/lib/providers.ts
@@ -16,9 +16,17 @@ import { DevToolConfig, NG_INAPP_DEV_TOOL_CONFIG } from './config.token';
 import { DevToolShellComponent } from './dev-tool-shell.component';
 
 // Import build in plugins
+import { OverviewComponent } from './plugins/overview/overview.component';
 
 // Setup build in plugins here
-const BUILT_IN_PLUGINS: Plugin[] = [];
+const BUILT_IN_PLUGINS: Plugin[] = [
+    {
+        name: 'Overview',
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>',
+        order: 0,
+        component: OverviewComponent
+    }
+];
 
 
 // Provider to return our plugins
